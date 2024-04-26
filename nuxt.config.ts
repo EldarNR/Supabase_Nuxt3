5a7d09e44947f5121c42fb7993fffe939b011a0d
Eldar <eldar121202@gmail.com>
2024-04-26 16:19:41 +0500

    routing for vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,13 @@ export default defineNuxtConfig({
        */
     },
   ],
+  routeRules: {
+    // Homepage pre-rendered at build time
+    "/": { prerender: true },
+    // Products page generated on demand, revalidates in background, cached until API response changes
+    "/store": { swr: true },
+    // Product page generated on demand, revalidates in background, cached for 1 hour (3600 seconds)
+  },
   supabase: {
     // Options
     redirectOptions: {
