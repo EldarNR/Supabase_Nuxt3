56898637f3132e765521c02ee05eeab46003b39f
Eldar <eldar121202@gmail.com>
2024-04-29 19:12:52 +0500

    fix 2 test vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       SUPABASE_PUBLIC_URL: process.env.SUPABASE_URL,
-      SUPABASE_KEY: process.env.SUPABASE_ANON_KEY,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
     },
   },
 });
