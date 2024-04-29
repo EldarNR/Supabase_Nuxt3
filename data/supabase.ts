afa98f824d5314e86d3ec35ec759d8fc7793d9aa
Eldar <eldar121202@gmail.com>
2024-04-29 19:16:53 +0500

    fix 3

--- data/supabase.ts
+++ data/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 const config = useRuntimeConfig();
 const supabase = createClient(
   config.public.SUPABASE_PUBLIC_URL as string,
-  config.public.supabase.key as string
+  config.public.SUPABASE_ANON_KEY as string
 );
 export default supabase;
